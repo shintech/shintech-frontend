2523ee25da8aae1e044998e6be0c61625681bdcd
Michael Prather <mprather1@gmail.com>
2017-07-23 23:26:20 +0000

    error checking

--- src/queries/models/index.js
+++ src/queries/models/index.js
@@ -4,12 +4,20 @@ import got from 'got'
 const queries = {}
 
 export default function getAllRoutes (options) {
-  const { serverAddress } = options
+  const { serverAddress, logger } = options
 
   queries.fetchAllModels = (req, res, next) => {
     let address = url.resolve(serverAddress, '/api/models')
     res.setHeader('Content-Type', 'application/json')
-    got.stream(address).pipe(res)
+    const stream = got.stream(address)
+    stream.pipe(res)
+
+    stream.on('error', err => {
+      logger.error(err)
+      res.send({
+        'error': err
+      })
+    })
   }
 
   return queries
